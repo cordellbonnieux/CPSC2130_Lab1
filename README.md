4ab61f5c0d80a4068331c109a7c74b88e990ec4a
Cordell <cordellbonnieux@gmail.com>
2022-01-20 00:06:30 -0800

    Refactored and reorganized index()

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-CPSC 2130 - Lab1
+Class: CPSC 2130
+Assignment: Lab 1
+Student: Cordell Bonnieux
 
-Class project; build a website about a city and include 3 pages and a 3d sprite spinner. 
+Objective: build a website about a city and include 3 pages and a 3d sprite spinner. 
--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
 // get root element, then append a container div
 const root = document.getElementById("root");
-const container = create("div", "container", root);
+const content = create("div", "content", root);
 
 // get all the images and backgrounds
 const backgrounds = {
-    0 : "./img/la-ville.jpg",
+    0 : "img/la-ville.jpg",
 }
 
 buildPage();
@@ -25,19 +25,37 @@ function buildPage() {
     }
 }
 
-// clean this up big time!
 function index() {
-    // set background
     document.body.style.backgroundImage = `url(${backgrounds[0]})`;
-    // create header section
-    const header = create("div", "header", root);
-    const inner = create("div", "inner", header);
-    // page title
-    const title = create("h1", "title", inner);
-    title.textContent = "Bienvenue a Montreal";
+    indexHeader();
+
+    const container = create("div", "container", content);
+
+    //create page
+    const page = create("div", "sectionWrapper", container);
+}
+
+function indexHeader() {
+    // create header content section
+    const section = create("div", "section", content);
+    const inner = create("div", "inner", section);
+
+    // landing title
+    const title = create("div", "landingTitleWrapper item", inner);
+    const titleH1 = create("h1", "landingTitle", title);
+    titleH1.textContent = "Bienvenue a Montreal";
 
     // create first call to action section
+    const news = create("div", "callToAction item", inner);
+    const newsLink = create ("a", "newsLink", news);
+    newsLink.setAttribute("href", "newsevents.html");
+    newsLink.textContent = "News Events";
+
     // create second call to action section
+    const rec = create("div", "callToAction item", inner);
+    const recLink = create("a", "recLink", rec);
+    recLink.setAttribute("href", "recreation.html");
+    recLink.textContent = "Recreation";
 }
 
 // returns a div with the given class name
@@ -46,6 +64,6 @@ function create(type, name, parent) {
     let ele = document.createElement(type);
     ele.setAttribute("class", name);
     parent.appendChild(ele);
-    return div;
+    return ele;
 }
 
